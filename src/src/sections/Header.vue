17909b6721dd7e0482dda0e3fb78ad0204688572
Haya <hayachannel5@gmail.com>
2024-05-06 17:46:36 +0900

    削除されたファイルの復元

---
+++ src/src/sections/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="header">Hashin.net</div>
+</template>
+
+<script>
+export default {
+  name: 'Header-section'
+}
+</script>
+
+<style>
+div#header {
+  margin: 1rem 1rem 0 1rem;
+  padding: 0 0 0 1rem;
+  border-bottom: #c4c4c4 solid 0.1rem;
+}
+</style>
