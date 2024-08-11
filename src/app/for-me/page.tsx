3acaf312a63f2a011c890edb1b8077a4ecf35729
Haya <hayachannel5@gmail.com>
2024-08-11 17:33:56 +0900

    feat: 自分用ページを最低限書く

--- src/app/for-me/page.tsx
+++ src/app/for-me/page.tsx
@@ -1,8 +1,22 @@
+import { SectionComponent } from "@/components/Utils";
+
 export default function ForMePage() {
   return (
-    <div>
-      <h1>For Me</h1>
-      <p>This is the for me page.</p>
-    </div>
+    <main>
+      <SectionComponent>
+        <h2>自分用ページ</h2>
+        <p>よく使うカラーパレットなどをまとめておく</p>
+      </SectionComponent>
+
+      <SectionComponent>
+        <h3>カラーパレット</h3>
+        <p>よく使うカラーパレット</p>
+      </SectionComponent>
+
+      <SectionComponent>
+        <h3>フォント</h3>
+        <p>よく使うフォント</p>
+      </SectionComponent>
+    </main>
   );
 }
